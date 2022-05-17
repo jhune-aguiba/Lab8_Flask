2280508c9765bfdcd78b0e8d578d21207038d0d1
jhune-aguiba <79142461+jhune-aguiba@users.noreply.github.com>
2022-05-17 14:40:47 +0800

    Chapter 3-4 Lab8

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +21,12 @@ def index():
 @app.route('/button')
 def button():
     return render_template("button.html", title="Button!")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
